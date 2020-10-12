543b4870deed7723e68f342d105b77131f7c65f3
zgaleday <zach.gale-day@ucsf.edu>
2020-10-12 09:54:41 -0700

    Implement RMSE loss and update function signature

--- pgn/train/train.py
+++ pgn/train/train.py
@@ -27,7 +27,7 @@ def train(model, data_loader, loss_function, optimizer, scheduler,
     for batch in tqdm(data_loader):
         batch = batch.to(device)
         optimizer.zero_grad()
-        loss = loss_function(model(_format_batch(train_args, batch)), batch)
+        loss = loss_function(model(_format_batch(train_args, batch)), batch.y, batch.num_graphs)
         loss.bachward()
         total_loss += loss.item() * batch.num_graphs
         optimizer.step()
--- pgn/train/train_utils.py
+++ pgn/train/train_utils.py
@@ -1,7 +1,19 @@
 from pgn.data.dmpnn_utils import BatchProxGraph
+import torch
 
 def _format_batch(train_args, data):
     if train_args.encoder_type == 'd-mpnn':
         return BatchProxGraph(data)
     else:
         return data
+
+
+def rmse_loss(predicted, actual, num_graphs):
+    """
+    Returns the RMSE loss for given predicted and ground_truth values for a given number of graphs (batch size)
+    :param predicted: The output of the model on the given batch of data
+    :param actual: The ground truth value for the given graphs
+    :param num_graphs: The number of graphs being evaluated
+    :return: The average RMSE loss over the given graphs.
+    """
+    return torch.sqrt(torch.sum((predicted - actual) ** 2) / num_graphs)

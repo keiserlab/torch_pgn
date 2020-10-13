b0d41eb8f501ba2ed416aa88d5ec55e524e3b3d3
zgaleday <zach.gale-day@ucsf.edu>
2020-10-13 10:34:18 -0700

    Implement distance norm and transform parser functions in data_utils

--- pgn/data/data_utils.py
+++ pgn/data/data_utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 import torch
+import torch_geometric.transforms as T
+from pgn.data.dmpnn_utils import MolGraphTransform
 
 class OneHotTransform(object):
     """
@@ -35,6 +37,33 @@ def normalize_targets(dataset, mean=None, std=None, yield_stats=False):
     dataset.data.y = (dataset.data.y - mean) / std
 
     if yield_stats:
-        return [dataset]
+        return [dataset.data.y]
     else:
-        return [dataset, (mean, std)]
+        return [dataset.data.y, (mean, std)]
+
+
+def normalize_dist(dataset, mean=None, std=None, yield_stats=False):
+    """
+    Normalizes the training target to have mean 0 and stddev 1
+    :param dataset: dataset to normalize the targets for
+    :param mean: external mean to use for normalization (i.e. test set normalization)
+    :param std: external stddev to use for normalization (i.e. test set normalization)
+    :param yield_stats: toggle to yield [dataset, (mean, std)] if True or just [dataset] if False
+    :return: A dataset with normalized targets
+    """
+    if mean is None:
+        mean = dataset.data.edge_attr[:, 0].mean()
+    if std is None:
+        std = dataset.data.edge_attr[:, 0].std()
+
+    dataset.data.edge_attr[:, 0] = (dataset.data.edge_attr[:, 0] - mean) / std
+
+    if yield_stats:
+        return [dataset.data.edge_attr[:, 0]]
+    else:
+        return [dataset.data.edge_attr[:, 0], (mean, std)]
+
+
+def parse_transforms(transforms):
+    valid_transforms = {'one_hot': OneHotTransform(), 'molgraph': MolGraphTransform()}
+    return T.Compose([valid_transforms.get(t) for t in transforms if t in valid_transforms.keys()])

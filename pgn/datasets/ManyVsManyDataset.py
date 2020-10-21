96b4adf7923caec18a2aff4226628b30aae4e71e
zgaleday <zach.gale-day@ucsf.edu>
2020-10-21 13:00:51 -0700

    Fix class definition

--- pgn/datasets/ManyVsManyDataset.py
+++ pgn/datasets/ManyVsManyDataset.py
@@ -12,26 +12,33 @@ import oddt.pandas as opd
 from pgn.graphs.graph_utils import _return_graph
 from pgn.datasets.PGDataset import PGDataset
 
-def ManyVsManyDataset(PGDataset):
+class ManyVsManyDataset(PGDataset):
     """
     This dataset is for a 1:1 receptor to ligand mapping. The paths (raw_pdb_path and raw_mol_path) are the same and
     point to a directory containing subdirectories containing each ligand receptor pair.
+    #TODO: Currently conforms to only pdbbind input formats. Make this much more generic.
     """
     def __init__(self, args):
         super(ManyVsManyDataset, self).__init__(args)
         self.raw_pdb_path = args.raw_pdb_path
         self.raw_mol_path = args.raw_mol_path
         self.num_workers = args.num_workers
+        self.label_file = args.label_file
+        self.label_col_name = args.label_col_name
 
     def process_raw_data(self):
-        receptor = next(oddt.toolkit.readfile('pdb', self.raw_pdb_path))
-        data = opd.read_mol2(self.raw_mol_path)
-        energy = data['Total Energy']
-        name = data['Name']
-        mol = data['mol']
+        raw_path = self.raw_pdb_path
+        directories = os.listdir(self.raw_pdb_path)
+
         inputs = []
-        for i, molecule in tqdm(enumerate(name), prefix="Formating raw data for graph creation"):
-            inputs.append((receptor, mol[i], energy[i], molecule))
+
+        for name in tqdm(directories):
+            if name not in ['index', 'readme', '.DS_Store']:
+                pdb_path = os.path.join(raw_path, name, name + "_protein.pdb")
+                ligand_path = os.path.join(raw_path, name, name + "_ligand.sdf")
+                receptor = next(oddt.toolkit.readfile('pdb', pdb_path))
+                ligand = opd.read_sdf(ligand_path)['mol'][0]
+                inputs.append((receptor, ligand, energy[i], name))
 
         with multiprocessing.Pool(processes=self.num_workers) as p:
             self.graphs = list(tqdm(p.imap(_return_graph, inputs)))

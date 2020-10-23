e64f735cf9f0a00834e4b7a3710532320dbf11c2
zgaleday <zach.gale-day@ucsf.edu>
2020-10-23 13:03:27 -0700

    Fix help string comments

--- pgn/args.py
+++ pgn/args.py
@@ -7,41 +7,53 @@ from typing_extensions import Literal
 class EncoderArgs(Tap):
     """Class used to store the arguments used for input to the message passing NN encoder."""
 
-    #In dimension to the NN_conv layer
+
     nn_conv_in_dim: int = 16
-    #Dimension of the internal ff_network in the NN_conv layer
+    """In dimension to the NN_conv layer"""
     nn_conv_internal_dim: int = 128
-    # output dimension of the nn_conv output matrix (nn_conv_out_dim x nn_conv_out_dim)
+    """Dimension of the internal ff_network in the NN_conv layer"""
     nn_conv_out_dim: int = 16
-    # type of pool used in the nn_conv layer
+    """output dimension of the nn_conv output matrix (nn_conv_out_dim x nn_conv_out_dim)"""
     nn_conv_aggr: Literal['add', 'mean', 'max'] = 'mean'
-    # pooling applied as part of readout function
+    """type of pool used in the nn_conv layer"""
     pool_type: Literal['add', 'mean', 'max'] = 'add'
-    # sparsification function used before final readout
+    """pooling applied as part of readout function"""
     sparse_type: Literal['log_softmax', 'softmax', 'none'] = 'log_softmax'
-    # whether or not to apply batch norm to the fp_layer before readout
+    """sparsification function used before final readout"""
     fp_norm: int = True
-    # The dimension of the output feature vector
+    """whether or not to apply batch norm to the fp_layer before readout"""
     fp_dim: int = 4096
-    # The number of message passing steps
+    """The dimension of the output feature vector"""
     depth: int = 3
-    # Toggle for whether to readout the fp vector at only time=T (False) or to aggregate all the feature
-    # vectors by adding the readout after each message passing step to the final readout vector.
+    """The number of message passing steps"""
     skip_connections: bool = True
+    """Toggle for whether to readout the fp vector at only time=T (False) or to aggregate all the feature
+        vectors by adding the readout after each message passing step to the final readout vector."""
 
 class FFArgs(Tap):
     """Class used to store the arguments used for input to the readout network."""
-    # probability of dropout applied between hidden layers
     dropout_prob: float = 0.0
-    # size of the FC layers
+    """probability of dropout applied between hidden layers"""
     hidden_dim: int = 400
-    # Number of FC layers
+    """size of the FC layers"""
     num_layers: int = 4
-    # Number of classes being predicted by the model
+    """Number of FC layers"""
     num_classes: int = 1
+    """Number of classes being predicted by the model"""
 
 class TrainArgs(Tap):
     """Class used to store the model independent arguments used for training the NNs"""
+    pass
+
+
+class DataArgs(Tap):
+    """
+    Class used to store the arguments used to construct and format the dataset being used
+    """
+    #### Probably will end up going into a shared arguments class, but for now keep as is####
+    seed: int = 0
+    """Random seed used for pytorch in order to ensure reproducibility"""
+
 
 class AggregatedArgs(Tap):
     """Wrapper class used to store the encoder, ff and train args classes"""

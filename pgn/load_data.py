f70c984dfb58783099ec1b3cb00136e10b7d7596
zgaleday <zach.gale-day@ucsf.edu>
2020-10-24 16:24:03 -0700

    Make load data work with dictionary for ease of debugging

---
+++ pgn/load_data.py
@@ -0,0 +1,21 @@
+from pgn.datasets.OneVsManyDataset import OneVsManyDataset
+from pgn.datasets.ManyVsManyDataset import ManyVsManyDataset
+from pgn.args import DataArgs
+
+
+args = DataArgs()
+# For parsing from terminal
+#args.parse_args()
+args.from_dict({'raw_data_path': '/Users/student/git/pgn/toy_data/ManyVsManyToy',
+                'data_path': '/Users/student/git/pgn/toy_out',
+                'dataset_type': 'many_v_many',
+                'split_type': 'random',
+                })
+args.process_args()
+
+if args.dataset_type == 'many_v_many':
+    ManyVsManyDataset(args)
+elif args.dataset_type == 'one_v_many':
+    OneVsManyDataset(args)
+else:
+    raise ValueError("Please input a valid dataset type.")

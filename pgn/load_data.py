77951dd60066d00cd204879a1ea7c51a68fdb238
zgaleday <zach.gale-day@ucsf.edu>
2020-10-29 21:10:06 -0700

    Finsih up load_data and make functional for cooperation with training scripts

--- pgn/load_data.py
+++ pgn/load_data.py
@@ -1,29 +1,42 @@
+import sys
+sys.path.append('~/pgn')
+sys.path.append('~/git/pgn/')
+
+import os.path as osp
+
 from pgn.datasets.OneVsManyDataset import OneVsManyDataset
 from pgn.datasets.ManyVsManyDataset import ManyVsManyDataset
 from pgn.data.ProximityGraphDataset import ProximityGraphDataset
-from pgn.data.data_utils import format_data_directory
+from pgn.data.data_utils import format_data_directory, split_test_graphs
 from pgn.args import DataArgs
 
 
-args = DataArgs()
-# For parsing from terminal
-#args.parse_args()
-args.from_dict({'raw_data_path': '/Users/student/git/pgn/toy_data/ManyVsManyToy',
-                'data_path': '/Users/student/git/pgn/toy_out',
-                'dataset_type': 'many_v_many',
-                'split_type': 'random',
-                'construct_graphs': False
-                })
-args.process_args()
+def process_raw(args: DataArgs):
+    if args.construct_graphs:
+        format_data_directory(args)
+        #Add check to see if dataset already processed.
+        if args.dataset_type == 'many_v_many':
+            ManyVsManyDataset(args)
+        elif args.dataset_type == 'one_v_many':
+            OneVsManyDataset(args)
+        else:
+            raise ValueError("Please input a valid dataset type.")
 
-if args.construct_graphs:
-    format_data_directory(args)
-    #Add check to see if dataset already processed.
-    if args.dataset_type == 'many_v_many':
-        ManyVsManyDataset(args)
-    elif args.dataset_type == 'one_v_many':
-        OneVsManyDataset(args)
-    else:
-        raise ValueError("Please input a valid dataset type.")
+    if args.split_type == 'defined':
+        split_test_graphs(args.data_path, args.label_file)
 
-dataset = ProximityGraphDataset(args.data_path)
+
+if __name__ == "__main__":
+
+    args = DataArgs()
+    # For parsing from terminal
+    # args.parse_args()
+    args.from_dict({'raw_data_path': '/Users/student/git/pgn/toy_data/ManyVsManyToy',
+                    'data_path': '/Users/student/git/pgn/toy_out',
+                    'dataset_type': 'many_v_many',
+                    'split_type': 'random',
+                    'construct_graphs': False
+                    })
+    args.process_args()
+
+    process_raw(args)

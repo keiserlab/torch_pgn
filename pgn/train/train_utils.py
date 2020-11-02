d904096a2a9097196a9bc168112bb02b3362baa1
zgaleday <zach.gale-day@ucsf.edu>
2020-11-02 11:20:30 -0800

    Write directory formating function for the output of training

--- pgn/train/train_utils.py
+++ pgn/train/train_utils.py
@@ -2,6 +2,9 @@ from pgn.data.dmpnn_utils import BatchProxGraph
 import torch
 import torch.nn.functional as F
 
+import os.path as osp
+import os
+
 def _format_batch(train_args, data):
     if train_args.encoder_type == 'd-mpnn':
         return BatchProxGraph(data)
@@ -39,3 +42,26 @@ def parse_loss(args):
     loss_possibilities = {'rmse': rmse_loss, 'mse': mse_loss}
     loss_function = args.loss_fucntion
     return lambda predicted, actual, num_grahps: loss_possibilities[loss_function](predicted, actual, num_grahps)
+
+
+def make_save_directories(save_directory):
+    """
+    Formats the empty save directory in order to have the proper format.
+    :param save_directory: An empty directory where the output of training will be saved.
+    :return: None
+    """
+    if len(os.listdir(save_directory)) != 0:
+        raise ValueError(
+            "The save directory is not empty. Please either clean the directory or specify and empty directory."
+        )
+    if not os.path.isdir(save_directory):
+        raise ValueError(
+            "The specified save directory does not exist. Please create an empty directory with this path or specify"
+            "a different path."
+        )
+    # Save the pytorch model data and the arguments json to this directory
+    model_dir = osp.join(save_directory, "model")
+    # The results of the training: i.e. data splits, predicted vs. actual for specified data sets, any plots specified etc.
+    results_dir = osp.join(save_directory, "results")
+    os.mkdir(model_dir)
+    os.mkdir(results_dir)

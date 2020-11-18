2beb21aae99c3dfa41fda20066dbe61bcafb1521
zgaleday <zach.gale-day@ucsf.edu>
2020-11-17 18:45:23 -0800

    Basic functionality in train class implemented

--- pgn/train/Trainer.py
+++ pgn/train/Trainer.py
@@ -4,6 +4,7 @@
 from pgn.train.train_model import train_model
 from pgn.load_data import process_raw
 from pgn.data.load_data import load_proximity_graphs
+from pgn.train.train_model import train_model
 
 class Trainer():
     """Class that loaders and holds the arguments and data objects. Allows for easy evaluation and retraining when the
@@ -12,6 +13,7 @@ class Trainer():
         #Need to see if this is going to work with subparsers
         self.args = args
         self.load_data()
+        self.model = None
 
     def load_data(self):
         if self.args.construct_graphs:
@@ -32,3 +34,19 @@ class Trainer():
         self.args = hyperopt_args
         if reload_data:
             self.load_data()
+
+    def run_training(self):
+        """
+        Runs
+        :return:
+        """
+        self.model, self.eval = train_model(self.args, self.train_data, self.valid_data)
+
+    def get_score(self):
+        """
+        Returns the score
+        :return:
+        """
+        if self.model is None:
+            raise RuntimeError("Score attempted to be retrieved before model trained.")
+        return self.eval[self.args.loss_function]

0598848b15aa75db411dfa2428ed5e3f5b22f0d4
zgaleday <zach.gale-day@ucsf.edu>
2020-12-19 12:30:02 -0800

    Support for cross_validation

--- pgn/train/cross_validate_model.py
+++ pgn/train/cross_validate_model.py
@@ -1,7 +1,17 @@
+from torch.utils.data import Subset
+import torch
+
 from sklearn.model_selection import KFold
+import numpy as np
+
+import os.path as osp
+import os
+
+from pgn.data.data_utils import normalize_targets, normalize_distance
+from pgn.train.train_model import train_model
 
 
-def cross_validation(args, train_data, test_data=None):
+def cross_validation(args, train_data):
     """
     Function to run cross validation to train a model.
     :param args: TrainArgs object containing the parameters for the cross-validation run.
@@ -12,9 +22,46 @@ def cross_validation(args, train_data, test_data=None):
     folds = args.cv_folds
     base_dir = args.save_dir
     seed = args.seed
+    norm_targets = args.normalize_targets
+    norm_dist = args.normalize_dist
 
     kfold = KFold(n_splits=folds, shuffle=True, random_state=seed)
-    train_examples = list(train_data.x.size())[1]
-    print(train_examples)
+    fold = 0
+    evals = []
+    for train_index, valid_index in kfold.split(train_data):
 
-    #for train_index, valid_index in kfold.split()
+        # Normalize targets and dist
+        if norm_targets:
+            label_mean = train_data.data.y[list(train_index)].mean()
+            label_std = train_data.data.y[list(train_index)].std()
+            args.label_mean, args.label_std = label_mean, label_std
+            train_data.data.y = (train_data.data.y - label_mean) / label_std
+
+        if norm_dist:
+            dist_mean = train_data.data.edge_attr[list(train_index), 0].mean()
+            dist_std = train_data.data.edge_attr[list(train_index), 0].std()
+            args.distance_mean, args.distance_std = dist_mean, dist_std
+            train_data.data.edge_attr[:, 0] = (train_data.data.edge_attr[:, 0] - dist_mean) / dist_std
+
+        # Split datasets
+        fold_train = train_data[list(train_index)]
+        fold_valid = train_data[list(valid_index)]
+
+        fold_dir = osp.join(base_dir, 'cv_fold_{0}'.format(fold))
+        args.save_dir = fold_dir
+        os.mkdir(fold_dir)
+
+        # Run training
+        model, eval = train_model(args, fold_train, fold_valid)
+        evals.append(eval)
+
+        # Revert train_dataset std and mean for next CV round
+        if norm_targets:
+            train_data.data.y = (train_data.data.y * label_std) + label_mean
+
+        if norm_dist:
+            train_data.data.edge_attr[:, 0] = (train_data.data.edge_attr[:, 0] * dist_std) + dist_mean
+
+        fold += 1
+
+    return model, evals

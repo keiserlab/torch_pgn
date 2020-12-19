4e2e6594c673416a796331d9ea348f15eb4c88fd
zgaleday <zach.gale-day@ucsf.edu>
2020-12-18 20:57:46 -0800

    Add basic cross_validation loading functionality

--- pgn/data/load_data.py
+++ pgn/data/load_data.py
@@ -5,6 +5,9 @@ from pgn.data.ProximityGraphDataset import ProximityGraphDataset
 
 import torch
 
+# TODO: Add a little bit more though into how we want to do defined splits. Probably have an option for only having
+#  a defined test set i.e. defined_test option in split type
+
 
 def load_proximity_graphs(args):
     """
@@ -21,6 +24,10 @@ def load_proximity_graphs(args):
     include_dist = args.include_dist
     norm_dist = args.normalize_dist
     load_test = args.load_test
+    cross_validate = args.cross_validate
+
+    if cross_validate:
+        return _load_data_cross_validation(args)
 
     torch.manual_seed(seed)
 
@@ -30,6 +37,7 @@ def load_proximity_graphs(args):
         train_dataset.data = transforms(train_dataset.data)
 
         num_examples = len(train_dataset.data.name)
+        #TODO: make this truly random by using shuffled indexing
 
         valid_begin, valid_end = 0, int(args.validation_percent * num_examples)
         test_begin, test_end = valid_end, int(args.test_percent * num_examples) + valid_end
@@ -39,7 +47,6 @@ def load_proximity_graphs(args):
         test_dataset = train_dataset[test_begin: test_end]
         train_dataset = train_dataset[train_begin:train_end]
 
-        #TODO: Add stats to args
         if norm_targets:
             train_dataset.data.y, target_stats = normalize_targets(train_dataset, yield_stats=True)
             args.label_mean, args.label_std = target_stats
@@ -89,3 +96,62 @@ def load_proximity_graphs(args):
 
     else:
         return train_dataset, validation_dataset
+
+
+def _load_data_cross_validation(args):
+    """
+    Helper function to keep main function from getting to complicated and clutter. Handles the loading and returning
+    or data when cross-validation will be used to train and evaluate/select the model.
+    :param args: The Argument object.
+    :return: The datasets specified by args (either train alone if load_test is set to False or both train and test).
+    """
+    data_path = args.data_path
+    seed = args.seed
+    # Add function to parse transforms into composed format for application
+    transforms = parse_transforms(args.transforms)
+    split_type = args.split_type
+    norm_targets = args.normalize_targets
+    include_dist = args.include_dist
+    norm_dist = args.normalize_dist
+    load_test = args.load_test
+
+    torch.manual_seed(seed)
+
+    if split_type == 'random':
+
+        train_dataset = ProximityGraphDataset(data_path, include_dist=include_dist)
+        train_dataset.data = transforms(train_dataset.data)
+
+        num_examples = len(train_dataset.data.name)
+        # TODO: make this truly random by using shuffled indexing
+
+        test_begin, test_end = 0, int(args.test_percent * num_examples)
+        train_begin, train_end = test_end, num_examples
+
+        test_dataset = train_dataset[test_begin: test_end]
+        train_dataset = train_dataset[train_begin:train_end]
+
+    elif split_type == 'defined':
+
+        train_begin, train_end = args.train_splits
+
+        train_dataset = ProximityGraphDataset(data_path, include_dist=include_dist)
+        train_dataset.data = transforms(train_dataset.data)
+
+        train_dataset = train_dataset[train_begin:train_end]
+
+        if load_test:
+            test_dataset = ProximityGraphDataset(data_path, include_dist=include_dist, mode='test')
+            test_dataset.data = transforms(test_dataset.data)
+
+    else:
+        raise ValueError('Invalid dataset type. Please choose from <random> or <defined>')
+
+    args.node_dim = train_dataset.data.x.numpy().shape[1]
+    args.edge_dim = train_dataset.data.edge_attr.numpy().shape[1]
+
+    if load_test:
+        return train_dataset, test_dataset
+
+    else:
+        return train_dataset

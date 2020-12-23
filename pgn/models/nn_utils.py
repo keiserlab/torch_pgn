4fb365c980c1c8b1e920c375b9194d418ab26653
zgaleday <zach.gale-day@ucsf.edu>
2020-12-22 21:43:56 -0800

    Add dmpnn encoder and helper to nn_utils

--- pgn/models/nn_utils.py
+++ pgn/models/nn_utils.py
@@ -27,3 +27,23 @@ def get_pool_function(pool_type: str):
         # TODO: add better error parsing/catching
         return Exception("{0} is an invalid pool_type. Please choose between 'add', 'mean' or 'max' pooling.".
                          format(pool_type))
+
+
+# From chemprop
+def index_select_ND(source, index):
+    """
+    Selects the message features from source corresponding to the atom or bond indices in :code:`index`.
+    :param source: A tensor of shape :code:`(num_bonds, hidden_size)` containing message features.
+    :param index: A tensor of shape :code:`(num_atoms/num_bonds, max_num_bonds)` containing the atom or bond
+                  indices to select from :code:`source`.
+    :return: A tensor of shape :code:`(num_atoms/num_bonds, max_num_bonds, hidden_size)` containing the message
+             features corresponding to the atoms/bonds specified in index.
+    """
+    index_size = index.size()  # (num_atoms/num_bonds, max_num_bonds)
+    suffix_dim = source.size()[1:]  # (hidden_size,)
+    final_size = index_size + suffix_dim  # (num_atoms/num_bonds, max_num_bonds, hidden_size)
+
+    target = source.index_select(dim=0, index=index.view(-1))  # (num_atoms/num_bonds * max_num_bonds, hidden_size)
+    target = target.view(final_size)  # (num_atoms/num_bonds, max_num_bonds, hidden_size)
+
+    return target

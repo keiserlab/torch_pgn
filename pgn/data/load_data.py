b89f1ef1d962be8c7464853e9251743da320a396
zgaleday <zach.gale-day@ucsf.edu>
2020-12-25 18:29:52 -0800

    Update load data.

--- pgn/data/load_data.py
+++ pgn/data/load_data.py
@@ -146,7 +146,7 @@ def _load_data_cross_validation(args):
 
     if split_type == 'random':
 
-        train_dataset = ProximityGraphDataset(data_path, include_dist=include_dist)
+        train_dataset = ProximityGraphDataset(args)
         train_dataset.data = transforms(train_dataset.data)
 
         num_examples = len(train_dataset.data.name)

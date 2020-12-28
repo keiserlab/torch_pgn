823398251095fd00003387e9a75fa696777a40e9
Zach Gale-Day <zgaleday@mk-1-a.compbio.ucsf.edu>
2020-12-27 22:21:25 -0800

    Fix error catching, need to handle .ds_store

--- pgn/data/data_utils.py
+++ pgn/data/data_utils.py
@@ -91,7 +91,7 @@ def parse_transforms(transforms):
 
 def format_data_directory(args):
     data_path = args.data_path
-    if len(os.listdir(data_path)) == 0:
+    if len(os.listdir(data_path)) != 0:
         raise ValueError("--data_path points to a non-empty directory. If the data has already been preprocessed. Please"
                          "use the --construct_graphs False argument")
     os.mkdir(osp.join(data_path, 'raw'))

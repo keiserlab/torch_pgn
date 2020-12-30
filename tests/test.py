f1470471478d4e5f02b12488dc4482642875b183
zgaleday <zach.gale-day@ucsf.edu>
2020-12-30 15:20:59 -0800

    Update testing

--- tests/test.py
+++ tests/test.py
@@ -2,11 +2,11 @@ from pgn.train.run_training import run_training
 from pgn.train.hyperopt import hyperopt
 from pgn.args import TrainArgs, HyperoptArgs
 
-def test_train():
+def test_pfp_train():
     args = TrainArgs()
 
-    args.from_dict({'raw_data_path': '/Users/student/git/pgn/tests/data/ManyVsManyToy',
-                    'data_path': '/Users/student/git/pgn/tests/data/toy_out',
+    args.from_dict({'raw_data_path': '/Users/student/git/pgn/tests/working_data/ManyVsManyToy',
+                    'data_path': '/Users/student/git/pgn/tests/working_data/toy_out',
                     'dataset_type': 'many_v_many',
                     'split_type': 'random',
                     'construct_graphs': False,
@@ -16,8 +16,6 @@ def test_train():
                     'test_percent': 0.2,
                     'save_splits': True,
                     'split_dir': '/Users/student/git/pgn/tests/splits',
-                    'memory_stats': True,
-                    'cv_folds': 2
                     })
     args.process_args()
 
@@ -27,19 +25,64 @@ def test_train():
 def test_hyperopt():
     args = HyperoptArgs()
 
-    args.from_dict({'raw_data_path': '/Users/student/git/pgn/tests/data/ManyVsManyToy',
-                    'data_path': '/Users/student/git/pgn/tests/data/toy_out',
+    args.from_dict({'raw_data_path': '/Users/student/git/pgn/tests/working_data/ManyVsManyToy',
+                    'data_path': '/Users/student/git/pgn/tests/working_data/toy_out',
                     'dataset_type': 'many_v_many',
                     'split_type': 'random',
                     'construct_graphs': False,
                     'save_dir': '/Users/student/git/pgn/tests/hyperopt_output',
                     'epochs': 2,
                     'save_splits': True,
-                    'cv_folds': 5,
+                    'cv_folds': 2,
                     'num_iters': 2
                     })
     args.process_args()
 
     hyperopt(args)
 
-test_train()
+def test_dmpnn_train():
+
+    args = TrainArgs()
+
+    args.from_dict({'raw_data_path': '/Users/student/git/pgn/tests/working_data/ManyVsManyToy',
+                    'data_path': '/Users/student/git/pgn/tests/working_data/toy_out',
+                    'dataset_type': 'many_v_many',
+                    'split_type': 'random',
+                    'construct_graphs': False,
+                    'save_dir': '/Users/student/git/pgn/tests/output',
+                    'epochs': 5,
+                    'validation_percent': 0.2,
+                    'test_percent': 0.2,
+                    'save_splits': True,
+                    'split_dir': '/Users/student/git/pgn/tests/splits',
+                    'encoder_type': 'dmpnn',
+                    'cv_folds': 2
+                    })
+    args.process_args()
+
+    run_training(args)
+
+
+def test_ggnet_train():
+
+    args = TrainArgs()
+
+    args.from_dict({'raw_data_path': '/Users/student/git/pgn/tests/working_data/ManyVsManyToy',
+                    'data_path': '/Users/student/git/pgn/tests/working_data/toy_out',
+                    'dataset_type': 'many_v_many',
+                    'split_type': 'random',
+                    'construct_graphs': False,
+                    'save_dir': '/Users/student/git/pgn/tests/output',
+                    'epochs': 5,
+                    'validation_percent': 0.2,
+                    'test_percent': 0.2,
+                    'save_splits': True,
+                    'split_dir': '/Users/student/git/pgn/tests/splits',
+                    'encoder_type': 'ggnet'
+                    })
+    args.process_args()
+
+    run_training(args)
+
+test_pfp_train()
+

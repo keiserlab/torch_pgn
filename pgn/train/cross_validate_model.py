820912b97d4d7061e88d84b7458c8cda2daaa8f3
zgaleday <zach.gale-day@ucsf.edu>
2021-01-02 17:40:25 -0800

    working splitting calculations

--- pgn/train/cross_validate_model.py
+++ pgn/train/cross_validate_model.py
@@ -73,8 +73,9 @@ def _split_dataset(index, fold, num_folds):
     :return: The train_index and validation_index
     """
     num_examples = len(index)
-    valid_percent = float(fold) / num_folds
-    valid_size = valid_percent * num_examples
+    valid_percent = 1 / num_folds
+    valid_size = int(valid_percent * num_examples)
+    print(num_examples, valid_percent, valid_size)
     valid_begin, valid_end = valid_size * fold, valid_size * fold + valid_size
     valid_index = index[valid_begin:valid_end]
     train_index = index

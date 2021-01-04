4250f1785aef345eef2d3b706ad52f5ec2babece
zgaleday <zach.gale-day@ucsf.edu>
2021-01-04 00:00:12 -0800

    Fix reshape in forward to get by batch for FF

--- pgn/models/FPEncoder.py
+++ pgn/models/FPEncoder.py
@@ -5,6 +5,9 @@ class FPEncoder(torch.nn.Module):
     """Pass-through encoder module for fp datasets."""
     def __init__(self, args: TrainArgs):
         super(FPEncoder, self).__init__()
+        self.args = args
+        self.fp_dim = args.fp_dim
+
 
     def forward(self, data):
-        return data.x
+        return data.x.reshape(data.num_graphs, self.fp_dim)

ef2ec175e8b2d0a541ac84847daaf951a5bba0c3
zgaleday <zach.gale-day@ucsf.edu>
2021-02-16 13:48:13 -0800

    Filter function

---
+++ scripts/filter_for_low_energy.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import oddt.pandas as opd
+
+TEST_FILE = '/srv/home/zgaleday/IG_data/raw_data/d4_test_compounds/experimally_test_chunkmap.csv'
+MOL_FILE = '/srv/home/zgaleday/IG_data/raw_data/d4_test_compounds/experimental_ds.mol2'
+OUTFILE = '/srv/home/zgaleday/IG_data/raw_data/d4_test_compounds/experimental_bestpos.mol2'
+
+test_df = pd.read_csv(TEST_FILE)
+mol2_df = opd.read_mol2(MOL_FILE, skip_bad_mols=True)
+print(mol2_df)
+mol2_df = mol2_df.groupby('mol_name')['Total Energy'].min()
+print(mol2_df)
+mol2_df.to_mol2(OUTFILE)

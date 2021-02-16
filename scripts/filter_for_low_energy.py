9602b6668dc2fd82a69f6c8efcef8d8974821d50
zgaleday <zach.gale-day@ucsf.edu>
2021-02-16 14:02:09 -0800

    Remove debugging lines

--- scripts/filter_for_low_energy.py
+++ scripts/filter_for_low_energy.py
@@ -8,10 +8,7 @@ OUTFILE = '/srv/home/zgaleday/IG_data/raw_data/d4_test_compounds/experimental_be
 
 test_df = pd.read_csv(TEST_FILE)
 mol2_df = opd.read_mol2(MOL_FILE, skip_bad_mols=True)
-print(mol2_df)
 mol2_df['best_energy'] = mol2_df.groupby(['mol_name'])['Total Energy'].transform(min)
-print(mol2_df)
 mol2_df = mol2_df[mol2_df['Total Energy'] == mol2_df['best_energy']]
 mol2_df.drop(columns='best_energy')
-print(mol2_df[['Total Energy', 'best_energy']])
 mol2_df.to_mol2(OUTFILE)

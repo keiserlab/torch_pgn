84530f0da9f10447fa9cacece31505dec4feaaa2
zgaleday <zach.gale-day@ucsf.edu>
2021-02-16 13:55:19 -0800

    Fix energy filtering

--- scripts/filter_for_low_energy.py
+++ scripts/filter_for_low_energy.py
@@ -9,6 +9,9 @@ OUTFILE = '/srv/home/zgaleday/IG_data/raw_data/d4_test_compounds/experimental_be
 test_df = pd.read_csv(TEST_FILE)
 mol2_df = opd.read_mol2(MOL_FILE, skip_bad_mols=True)
 print(mol2_df)
-mol2_df = mol2_df.groupby('mol_name')['Total Energy'].min()
+mol2_df['best_energy'] = mol2_df.groupby(['mol_name'])['Total Energy'].transform(min)
+print(mol2_df)
+mol2_df = mol2_df[mol2_df['Total_Energy'] == mol2_df['best_energy']]
+mol2_df.drop(['best_energy'])
 print(mol2_df)
 mol2_df.to_mol2(OUTFILE)

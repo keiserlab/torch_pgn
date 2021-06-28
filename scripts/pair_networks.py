eb8eb90d1957bf55548de5a8a78613ee30b2f3e6
zgaleday <zach.gale-day@ucsf.edu>
2021-06-28 10:19:06 -0700

    Format CM labels

--- scripts/pair_networks.py
+++ scripts/pair_networks.py
@@ -95,11 +95,11 @@ def calculate_confusion_matrix(args, classifier):
     experimental_df = pd.read_csv(experimental_path)
     train = experimental_df[experimental_df['set'] == 'train']
     test = experimental_df[experimental_df['set'] == 'test']
-    plot_confusion_matrix(classifier, test[['x', 'y']], test[['labels']], display_labels=["Non Binder", "Binder"])
+    plot_confusion_matrix(classifier, test[['x', 'y']], test[['labels']], display_labels=["Non\nBinder", "Binder"])
     plt.tight_layout()
     plt.savefig(osp.join(args.save_dir, 'results', 'classifier_CM_test.png'))
     plt.close()
-    plot_confusion_matrix(classifier, train[['x', 'y']], train[['labels']], display_labels=["Non Binder", "Binder"])
+    plot_confusion_matrix(classifier, train[['x', 'y']], train[['labels']], display_labels=["Non\nBinder", "Binder"])
     plt.tight_layout()
     plt.savefig(osp.join(args.save_dir, 'results', 'classifier_CM_train.png'))
     plt.close()

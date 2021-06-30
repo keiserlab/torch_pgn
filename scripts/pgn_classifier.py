d845c0b6fa0a0cb9ef76193a12405a920225549f
zgaleday <zach.gale-day@ucsf.edu>
2021-06-30 11:16:56 -0700

    Change main method signature

--- scripts/pgn_classifier.py
+++ scripts/pgn_classifier.py
@@ -38,7 +38,7 @@ from torch.utils.tensorboard import SummaryWriter
 LABEL_FILE = '/srv/home/zgaleday/IG_data/raw_data/d4_test_compounds/experimally_test_chunkmap.csv'
 
 
-def run_pair_network(checkpoint_path, dataset_path, savedir, device, epochs, repeats=5):
+def run_classifier(checkpoint_path, dataset_path, savedir, device, epochs, repeats=5):
     for iter in range(repeats):
         args, full_dataset_path = load_args(checkpoint_path, dataset_path, savedir, device, epochs)
         base_dir = savedir

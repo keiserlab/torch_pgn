c461697ab539e8bee951a97544dffb98fee22883
zgaleday <zach.gale-day@ucsf.edu>
2021-08-18 21:40:21 -0700

    Update chunk parse for grouped zincs

--- scripts/extract_chunks.py
+++ scripts/extract_chunks.py
@@ -7,12 +7,12 @@ import shutil
 import oddt.pandas as opd
 from tqdm import tqdm
 
-TEST_FILE = '/srv/home/zgaleday/IG_data/d4_test_compounds/experimally_test_chunkmap.csv'
+TEST_FILE = '/srv/home/zgaleday/IG_data/d4_test_compounds/900k_diverse_chunk_map.csv'
 SCREEN_DIRECTORY = '/srv/nas/mk2/projects/D4_screen/'
 TEST_CHUNK = 'vs_run1_chunk28150'
-OUTDIR = '/srv/home/zgaleday/IG_data/d4_test_compounds/'
+OUTDIR = '/srv/home/zgaleday/IG_data/raw_data/d4_900k_diverse'
 
-def parse_chunk(chunk_id, mol_name):
+def parse_chunk(chunk_id, mol_names):
     temp_file = osp.join(OUTDIR, 'temp.mol2')
     with gzip.open(osp.join(SCREEN_DIRECTORY, chunk_id, 'test.mol2.gz'), 'rb') as f:
         with open(temp_file, 'wb') as f_out:
@@ -20,14 +20,14 @@ def parse_chunk(chunk_id, mol_name):
     chunk = opd.read_mol2(temp_file)
     chunk['mol_name'] = chunk['mol_name'].apply(lambda x: x.split()[0])
     os.remove(temp_file)
-    return chunk[chunk['mol_name'] == mol_name]
+    return chunk[chunk['mol_name'].isin(mol_names)]
 
 exp_df = pd.read_csv(TEST_FILE)
+grouped_df = exp_df.groupby('chunk')['name'].apply(list)
 
 mol2_list = []
-for zid, chunk in tqdm(zip(exp_df['ZINC ID'], exp_df['chunk'])):
-    print(zid)
-    mol2_list.append(parse_chunk(chunk, zid))
+for chunk in tqdm(grouped_df.index):
+    mol2_list.append(parse_chunk(chunk, grouped_df[chunk]))
 
 
 
@@ -35,4 +35,4 @@ for zid, chunk in tqdm(zip(exp_df['ZINC ID'], exp_df['chunk'])):
 full_mol2 = pd.concat(mol2_list)
 print(full_mol2)
 
-full_mol2.to_mol2(osp.join(OUTDIR, 'experimental_ds.mol2'))
+full_mol2.to_mol2(osp.join(OUTDIR, 'diverse_900k_ds.mol2'))

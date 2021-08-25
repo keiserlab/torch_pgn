fc978d76ad77bfd6ff9b1fa6339683ebe87602eb
zgaleday <zach.gale-day@ucsf.edu>
2021-08-25 16:51:31 -0700

    Change path for low energy filter

--- scripts/filter_for_low_energy.py
+++ scripts/filter_for_low_energy.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import oddt.pandas as opd
 
-TEST_FILE = '/srv/home/zgaleday/IG_data/raw_data/d4_900k_diverse/900k_diverse_chunk_map_-61_to_-55.csv'
-MOL_FILE = '/srv/home/zgaleday/IG_data/raw_data/d4_900k_diverse/diverse_-55to-61_ds.mol2'
-OUTFILE = '/srv/home/zgaleday/IG_data/raw_data/d4_test_compounds/diverse_-55to-61_bestpos.mol2'
+TEST_FILE = '/srv/home/zgaleday/IG_data/raw_data/d4_900k_diverse/900k_diverse_chunk_map_-55_to_-49.csv'
+MOL_FILE = '/srv/home/zgaleday/IG_data/raw_data/d4_900k_diverse/diverse_900k_ds_-55to-49.mol2'
+OUTFILE = '/srv/home/zgaleday/IG_data/raw_data/d4_test_compounds/diverse_900k_ds_-55to-49_bestpos.mol2'
 
 test_df = pd.read_csv(TEST_FILE)
 mol2_df = opd.read_mol2(MOL_FILE, skip_bad_mols=True)

50314b12b51bd3ce3b3e620b96c309ea38910e4b
zgaleday <zach.gale-day@ucsf.edu>
2022-01-05 22:06:29 -0800

    Modularize filter_low_energy

--- scripts/filter_for_low_energy.py
+++ scripts/filter_for_low_energy.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np
 import oddt.pandas as opd
+import sys
 
-TEST_FILE = '/srv/home/zgaleday/IG_data/raw_data/d4_900k_diverse/900k_diverse_chunk_map.csv'
-MOL_FILE = '/srv/home/zgaleday/IG_data/raw_data/d4_900k_diverse/diverse_900k.mol2'
-OUTFILE = '/srv/home/zgaleday/IG_data/raw_data/d4_900k_diverse/diverse_900k_bestpos.mol2'
+def low_energy_filter(mol_file, out_file):
+    mol2_df = opd.read_mol2(mol_file, skip_bad_mols=True)
+    mol2_df['best_energy'] = mol2_df.groupby(['mol_name'])['Total Energy'].transform(min)
+    mol2_df = mol2_df[mol2_df['Total Energy'] == mol2_df['best_energy']]
+    mol2_df.drop(columns='best_energy')
+    mol2_df.to_mol2(out_file)
 
-test_df = pd.read_csv(TEST_FILE)
-mol2_df = opd.read_mol2(MOL_FILE, skip_bad_mols=True)
-mol2_df['best_energy'] = mol2_df.groupby(['mol_name'])['Total Energy'].transform(min)
-mol2_df = mol2_df[mol2_df['Total Energy'] == mol2_df['best_energy']]
-mol2_df.drop(columns='best_energy')
-mol2_df.to_mol2(OUTFILE)
+
+if __name__ == "__main__":
+    mol_file = sys.argv[1]
+    out_file = sys.argv[2]
+    low_energy_filter(mol_file, out_file)

ab4efd65c4809d909fb19a9102fedb24f3a73f89
zgaleday <zach.gale-day@ucsf.edu>
2023-05-01 14:25:10 -0700

    Complete graph sorting and position checking algorithms

--- tests/test.py
+++ tests/test.py
@@ -6,6 +6,11 @@ from pgn.load_data import process_raw
 from scripts.generate_plec import generate_plec_pdbbind
 from scripts.generate_final_correlations import generate_final_correlations
 
+import os
+import os.path as osp
+import numpy as np
+import networkx as nx
+
 def test_many_v_many_dataloading():
 
     args = DataArgs()
@@ -258,5 +263,85 @@ def test_split_conv():
 
     run_training(args)
 
-test_many_v_many_dataloading()
+#TODO: Split into seperate helper methods file
+def _load_graph(path, name):
+    """
+    Load graph from raw numpy files for comparision in the compare graph method
+    :param path: Path to the directory containing the raw graph saved as np files
+    :return: graph as a networkx object
+    """
+    edge_features = np.load(osp.join(path, f'{name}_edge_features.npy'))
+    edges = np.load(osp.join(path, f'{name}_edges.npy'))
+    label = np.load(osp.join(path, f'{name}_label.npy'))
+    node_features = np.load(osp.join(path, f'{name}_node_features.npy'))
+    node_pos = np.load(osp.join(path, f'{name}_pos3d.npy'))
+    G = nx.Graph()
+    for i in range(len(node_features)):
+        features = node_features[i]
+        pos = node_pos[i]
+        G.add_node(i, features=features, pos3d=pos)
+    for i in range(edges.shape[1]):
+        u, v = edges[0, i], edges[1, i]
+        features = edge_features[i]
+        G.add_edge(u, v, features=features)
+    G.graph['label'] = label
+    return G
 
+def _reorder_graph(G):
+    """
+    Function that takes a proximity graph and relabels the nodes such that it is numbered from smallest pos3d attribute
+    to largest.
+    :param G: Proximity Graph object
+    :return: Copy of the graph with the nodes sorted as described
+    """
+    nodes_pos_dict = nx.get_node_attributes(G, 'pos3d')
+    node_nums = sorted(nodes_pos_dict.keys())
+    node_pos_list = np.array([nodes_pos_dict[k] for k in node_nums])
+    sort_ind = np.lexsort((node_pos_list[:, 2], node_pos_list[:, 1], node_pos_list[:, 0]))
+    node_map = {sort_ind[i]: i for i in range(len(node_nums))}
+    H = nx.relabel_nodes(G, node_map)
+    return H
+
+
+def _compare_graphs(graph1, graph2, epsilon=0.001):
+    """
+    Compare proximity graphs and returns boolean for identity as defined as isomorphism and attribute matching.
+    :param graph1: comparison graph 1 (networkx)
+    :param G1: comparison graph 2 (networkx)
+    :return:
+    """
+    sorted_g1 = _reorder_graph(graph1)
+    sorted_g2 = _reorder_graph(graph2)
+    # Compare graph size
+    assert len(sorted_g1.nodes()) == len(sorted_g2.nodes())
+    #compare 3d coordinate
+    g1_pos = nx.get_node_attributes(sorted_g1, 'pos3d')
+    g2_pos = nx.get_node_attributes(sorted_g2, 'pos3d')
+    distance_matrix = [np.sqrt(np.sum(np.square(g1_pos[i] - g2_pos[i]))) for i in g1_pos.keys()]
+    # TODO: Switch from global epsilon to local epsilon with node level reporting
+    assert np.sum(distance_matrix) < epsilon
+
+
+def _compare_graph_dataset(truth_dataset, test_dataset):
+    """
+    Compare two raw datasets loaded using either the OneVsManyDataset or ManyVsManyDataset classes. Requires a
+    ground truth dataset and a dataset that you want to test for functional equiv. General usage is expect to be for
+    changes made to graph loading. i.e. make change and ensure no difference in graph results.
+    :param truth_dataset: path to top level of ground truth dataset generated before changes implemented
+    :param test_dataset: path to top level of dataset generated after changed to dataset generation have been implemented
+    :return: Boolean of whether datasets are functionally equivalent
+    """
+    #Test whether all molecule/receptor graphs are generated
+    #TODO: change to testing from assert
+    gt_graphs = os.listdir(osp.join(truth_dataset, 'raw', 'train'))
+    test_graphs = os.listdir(osp.join(test_dataset, 'raw', 'train'))
+    assert len(gt_graphs) == len(test_graphs), "Number of graphs not equal in new and ground truth dataset"
+    for tg in test_graphs:
+        assert tg in gt_graphs, f'{tg} not found in ground truth graphs'
+        test_graph = _load_graph(osp.join(osp.join(test_dataset, 'raw', 'train', tg)), tg)
+        gt_graph = _load_graph(osp.join(osp.join(truth_dataset, 'raw', 'train', tg)), tg)
+        print(_compare_graphs(test_graph, gt_graph))
+        break
+
+
+_compare_graph_dataset('ground_truth_dataset/mvm_toy_out', 'output_data/mvm_toy_dataset')

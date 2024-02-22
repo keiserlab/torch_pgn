1389e42a198307bd04178f76e9875b48e9fe7aae
Zach Gale-Day <zgaleday@mk-gpu-3.c.keiserlab.org>
2024-02-22 09:45:10 -0800

    Add updated pip install file

--- setup.py
+++ setup.py
@@ -9,10 +9,40 @@ with open("README.md", encoding="utf-8") as f:
 setup(
     name='pgn',
     version='1.0.0',
-    description='A description of your package',
-    author='Zachary Gale-Day',
-    author_email='z.gale.day@gmail.com',
+    description='Proximity Graph Networks: Predicting ligand affinity with Message Passing Neural Networks',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/keiserlab/pgn",
+    author='Keiser Lab',
+    author_email='keiser@keiserlab.org',
     license="MIT",
     packages=find_packages(),
-    install_requires=[],  # List your package dependencies here
+    install_requires=[
+        "torch>=1.7.0",
+        "torch_geometric>=1.6.3",
+        "networkx>=2.5",
+        "typed-argument-parser==1.6.1",
+        "pandas==1.1.5",
+        "rdkit>=2020.09.3",
+        "numpy>=1.19.2,<=1.21.6",
+        "tqdm>=4.54.1",
+        "tensorboard>2.0",
+        "scipy<1.7",
+        "scikit-learn>=0.23.2",
+        "hyperopt>=0.2.5"
+    ],
+    python_requires=">3.6,<3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=[
+        "chemistry",
+        "machine learning",
+        "affinity prediction",
+        "message passing neural network",
+        "graph neural network",
+    ],
+    
 )

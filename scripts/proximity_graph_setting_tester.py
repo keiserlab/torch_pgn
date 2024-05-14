ee7bb5d2b019b143832a4d787115160f67db5e10
zgaleday <zach.gale-day@ucsf.edu>
2024-05-13 18:27:12 -0700

    Add model training section

--- scripts/proximity_graph_setting_tester.py
+++ scripts/proximity_graph_setting_tester.py
@@ -3,6 +3,7 @@ sys.path.insert(0, "/srv/home/zgaleday/pgn")
 
 from pgn.args import TrainArgs, HyperoptArgs, DataArgs
 from pgn.load_data import process_raw
+from scripts.generate_final_correlations import generate_final_correlations
 
 import os
 import os.path as osp
@@ -31,11 +32,28 @@ def generate_datasets(raw_path, raw_label_file, data_path, dataset_type,
 
         process_raw(args)
 
+
+def generate_repeats(data_path, checkpoint_path, save_dir, split_dir, device, epoch=250):
+    for dir in os.listdir(data_path):
+        current_dir = osp.join(save_dir, dir)
+        data_dir = osp.join(data_path, dir)
+        generate_final_correlations(checkpoint_path, current_dir, split_dir, device, data_path=data_dir)
+
+
 if __name__ == '__main__':
     raw_data_path = sys.argv[1]
     raw_label_file = sys.argv[2]
     data_path = sys.argv[3]
     dataset_type = 'many_v_many'
-    if len(sys.argv) > 4:
+    if len(sys.argv) == 5:
         dataset_type = sys.argv[4]
-    generate_datasets(raw_data_path, raw_label_file, data_path, dataset_type)
+    try:
+        generate_datasets(raw_data_path, raw_label_file, data_path, dataset_type)
+    except:
+        pass
+    if len(sys.argv) > 5:
+        checkpoint_dir = sys.argv[5]
+        model_path = sys.argv[6]
+        split_dir = sys.argv[7]
+        device = sys.argv[8]
+        generate_repeats(data_path, checkpoint_dir, model_path, split_dir, device)

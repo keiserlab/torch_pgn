11bf727ee89e31a97a78cf061a3a765a3a8ee21e
zgaleday <zach.gale-day@ucsf.edu>
2024-05-17 18:13:37 -0700

    Update to run with dimenet

--- scripts/pdb_bind_random_hyperopt.py
+++ scripts/pdb_bind_random_hyperopt.py
@@ -7,23 +7,28 @@ from pgn.args import TrainArgs, HyperoptArgs
 
 args = HyperoptArgs()
 
+search_keys = ['num_blocks', 'int_emb_size', 'nn_conv_internal_dim',
+        'basis_emb_size', 'out_emb_channels']
+
 args.from_dict({'raw_data_path': '/srv/home/zgaleday/pdbbind_general_raw',
                 'label_file': '/srv/home/zgaleday/pdbbind_general_raw/index/INDEX_general_PL_data.2019',
-                'data_path': '/srv/home/zgaleday/IG_data/pdbbind_general_pgn/',
+                'data_path': '/srv/home/zgaleday/IG_data/pyg_update_ds/pdbbind_refined_2019/',
                 'dataset_type': 'many_v_many',
+                'encoder_type': 'dimenet++',
                 #'split_type': 'random',
                 'construct_graphs': False,
-                'save_dir': '/srv/home/zgaleday/models/pgn/figure_2/general_final_pgn_protein_splits',
-                'device': 'cuda:7',
-                'epochs': 200,
-                'cv_folds': 5,
+                'save_dir': '/srv/home/zgaleday/models/pgn/review_experiments/final_dimenet_hyperopt',
+                'device': 'cuda:0',
+                'epochs': 250,
+                'cv_folds': 3,
                 'save_splits': True,
-                'num_iters': 20,
-		'num_workers': 0,
-		'batch_size': 128,
-		'weight_decay': True,
+                'num_iters': 25,
+		        'num_workers': 0,
+		        'batch_size': 128,
+		        'weight_decay': True,
                 'split_type': 'defined_test',
-                'split_dir': '/srv/home/zgaleday/IG_data/general_protein_splits'
+                'split_dir': '/srv/home/zgaleday/models/pgn/figure2/refined_final_pgn/splits',
+                'search_keys': search_keys
                 })
 args.process_args()
 
